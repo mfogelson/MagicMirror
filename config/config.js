57fec7751ee3916b41cd500aeb53a1b3ffb4a602
mfogelson <mfogelson630@gmail.com>
2019-04-20 08:06:08 -0500

    added my config file and updated gitignore for config folder

---
+++ config/config.js
@@ -0,0 +1,112 @@
+/* Magic Mirror Config Sample
+ *
+ * By Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ *
+ * For more information how you can configurate this file
+ * See https://github.com/MichMich/MagicMirror#configuration
+ *
+ */
+
+var config = {
+	address: "localhost", // Address to listen on, can be:
+	                      // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+	                      // - another specific IPv4/6 to listen on a specific interface
+	                      // - "", "0.0.0.0", "::" to listen on any interface
+	                      // Default, when address config is left out, is "localhost"
+	port: 8080,
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
+	                                                       // or add a specific IPv4 of 192.168.1.5 :
+	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+	                                                       // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+
+	zoom: 0.5,
+	language: "en",
+	timeFormat: 24,
+	units: "metric",
+
+	modules: [
+		{
+			module: "alert",
+		},
+		{
+			module: "updatenotification",
+			position: "top_bar"
+		},
+		{
+			module: "clock",
+			position: "top_left",
+			config: {
+				timezone: 'Asia/Tokyo'
+			}
+		},
+		/*{
+			module: "calendar",
+			header: "Japan Holidays",
+			position: "top_left",
+			config: {
+				calendars: [
+					{
+						symbol: "calendar-check",
+						url: "webcal://www.calendarlabs.com/ical-calendar/ics/54/Japan-Holidays.ics"
+					}
+				]
+			}
+		},*/
+		{
+			module: "compliments",
+			position: "lower_third"
+		},
+	/*	{
+			module: "currentweather",
+			position: "top_right",
+			config: {
+				location: "Tokyo",
+				locationID: "",  //ID from http://bulk.openweathermap.org/sample/; unzip the gz file and find your city
+				appid: "YOUR_OPENWEATHER_API_KEY"
+			}
+		},
+		{
+			module: "weatherforecast",
+			position: "top_right",
+			header: "Weather Forecast",
+			config: {
+				location: "Tokyo",
+				locationID: "1850147",  //ID from https://openweathermap.org/city
+				appid: "YOUR_OPENWEATHER_API_KEY"
+			}
+		},*/
+		{
+			module:'MMM-forecast-io',
+			position: 'top_right',
+			config: {
+				apiKey: '3c0c920745a8cf01abd7984c2b4cacfb',
+				latitude: 35.34213,
+				longitude: 139.34480,
+				units: 'metric',
+				language: 'en',
+				maxDaysForecast: 3
+			}
+		},
+				
+		{
+			module: "newsfeed",
+			position: "bottom_bar",
+			config: {
+				feeds: [
+					{
+						title: "New York Times",
+						url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true
+			}
+		},
+	]
+
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {module.exports = config;}
